2002005fec74a190441a25d54ba066249cb5bc5f
Jeff Lander <beachgrub@gmail.com>
2018-03-06 16:03:41 -0800

    added deployment for sale contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var LicenseManager = artifacts.require("LicenseManager");
+var LicenseSale = artifacts.require("LicenseSale");
 
 module.exports = function(deployer) {
-  deployer.deploy(LicenseManager);
+  // Deploy LicenseManager, then deploy LicenseSale, passing in LicenseManager's newly deployed address
+  deployer.deploy(LicenseManager).then(function() {
+    return deployer.deploy(LicenseSale, LicenseManager.address);
+  });
 };
