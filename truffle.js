41216c31efd9110afc6fea30e0d0a3e0514ef730
Jeff Lander <beachgrub@gmail.com>
2018-03-07 08:49:54 -0800

    updated deployment

--- truffle.js
+++ truffle.js
@@ -1,8 +1,34 @@
+/*
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
+*/
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "finger uncover spoon banner cart patch warfare diary outer two receive modify";
+module.exports = {
+  networks: {
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/op3aLhRH781K9MoS9dFp")
+      },
+      network_id: 3
+    },	
+    ropsten:  {
+		  provider: function() {
+			return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/op3aLhRH781K9MoS9dFp")
+		  },
+		network_id: 1,
+		gas: 4000000,
+	},
+	development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
